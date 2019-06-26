47b1c639448319a38ac3342769df91be48059913
ppereira1 <paul.pereira@alyf.de>
2019-06-26 16:17:43 +0100

    Add  Button/Create Rows

--- event_management/event_management/doctype/block/block.js
+++ event_management/event_management/doctype/block/block.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Block', {
-	refresh: function(frm) {
+	refresh: function (frm) {
+		frm.add_custom_button(__("Create Rows"), function () {
+			frappe.call({
+				method: "event_management.event_management.doctype.block.block.create_rows",
+				args: {
+					amount: frm.doc.number_of_rows,
+					block: frm.doc.name
+				}
+			});
+		});
 
 	}
 });
